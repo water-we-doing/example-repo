a51b0b06356426dbb35278edbce1870a74bf6ed1
K. Dana Chadwick <kdchadwick@gmail.com>
2021-06-22 14:30:10 -0500

    updating greeting print statement

--- practice_code.py
+++ practice_code.py
@@ -1,9 +1,7 @@
 import numpy as np
 import pandas as pd
 
-print('starting over!')
-
-# print('\n \n \n \n Hello! Dana has contributed to this code file!\n \n \n \n ')
+print('\n \n \n \n Hello! Dana has contributed to this code file!\n \n \n \n ')
 
 # print('I am making this code way WAY better and more fancy')
 
