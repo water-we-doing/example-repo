7d9db3125b85fe1288bea7f74b137bd48fae08b1
K. Dana Chadwick <kdchadwick@gmail.com>
2021-06-22 11:22:20 -0500

    making print statements easier to read

--- practice_code.py
+++ practice_code.py
@@ -1,6 +1,4 @@
 import numpy as np
 import pandas as pd
 
-print('testing .py file')
-
-print('Hello! Dana has contributed to this code file!')
+print('\n \n \n \n Hello! Dana has contributed to this code file!\n \n \n \n ')
